19031a0e8402cb8a6a5cb7d062aed5dd2752abdb
Wolfgang Schreurs <wolfgang@wolftrail.net>
2021-10-17 21:01:27 +0700

    Code clean-up

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
---local DiamondSquare = require 'diamondsquare'
-
 local DiamondSquare = require 'diamondsquare'
 
 math.randomseed(os.time())
@@ -7,18 +5,11 @@ math.randomseed(os.time())
 -- show live output in console, don't wait for app to close
 io.stdout:setvbuf("no")
 
-local size = 4
-local map = nil
+local size = 8
 
-function love.load()
-	love.window.setTitle('Genesis')
+local maps = {}
 
-	local _ = love.window.setMode(1280, 800, {})
-
-	local vmin, vmax = 1.0, 0.0
-
-	map = DiamondSquare.create(2)
-
+local function printMap(map)
 	local s = ''
 	for x = 0, map.w do
 		for y = 0, map.h do
@@ -30,5 +21,36 @@ function love.load()
 	print(s)
 end
 
+function love.load()
+	love.window.setTitle('Genesis')
+
+	local _ = love.window.setMode(1280, 800, {})
+
+	local vmin, vmax = 1.0, 0.0
+
+	for i = 1, 6 do
+		map = DiamondSquare.create(size)
+		maps[#maps + 1] = map
+
+		map.ox = (i - 1) % 4
+		map.oy = math.floor((i - 1) / 4)
+
+		--printMap(map)
+	end
+
+end
+
 function love.draw()
+	for i, map in ipairs(maps) do
+		for x = 0, map.w do
+			for y = 0, map.h do
+				local v = map[x][y]
+				local xi = x + (map.ox * map.w) + (map.ox * 2)
+				local yi = y + (map.oy * map.h) + (map.oy * 2)
+
+				love.graphics.setColor(v, v, v, 1.0)
+				love.graphics.points(xi, yi)
+			end
+		end
+	end
 end

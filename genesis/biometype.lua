32200a37c4c4f352a23993201f5c404afdd88d95
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2023-11-15 22:32:59 +0700

    Add height colors & height type table

--- genesis/biometype.lua
+++ genesis/biometype.lua
@@ -9,6 +9,6 @@ return {
 	TROPICAL_RAINFOREST = 8,
 	SEASONAL_FOREST = 9,
 	TEMPERATE_RAINFOREST = 10,
-	SHALLOW_WATER = 11,
-	DEEP_WATER = 12,
+	SHALLOW_WATER = 11, -- TODO: maybe remove as overlap with HeightType
+	DEEP_WATER = 12, -- TODO: maybe remove as overlap with HeightType
 }
---
+++ genesis/heighttype.lua
@@ -0,0 +1,9 @@
+return {
+    DEEP_WATER = 1, -- (0.2), ocean
+    SHALLOW_WATER = 2, -- (0.4), sea
+    SAND = 3 , -- (0.5), beach
+    GRASS = 4, -- (0.7), 
+    FOREST = 5, -- (0.8), 
+    ROCK = 6, -- (0.9), 
+    SNOW = 7, -- (1.0),
+}
